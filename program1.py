0cf768dd59e2eb94d0c2dddaf2e65de06ab5b314
sankalp1672003 <sankalpgoyal1672003@gmail.com>
2024-11-11 13:32:31 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,11 +1,22 @@
 class Solution:
-    def dfs(r,c):
-        if 0<=r
+    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
         if not grid:
             return 0
         r,c=len(grid),len(grid[0])
         island=0
-                   
-        return 0
+        def dfs(row,column):
+            if 0<=row<r and 0<=column<c and grid[row][column]=='L':
+                grid[row][column]='W'
+                dfs(row-1,column)
+                dfs(row+1,column)
+                dfs(row,column-1)
+                dfs(row,column+1)
+        for row in range(r):
+            for column in range(c):
+                if grid[row][column]=='L':
+                    island+=1
+                    dfs(row,column)
+        return island
+        
